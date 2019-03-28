af34298190f7061b721e0d17a8d89d5bb4508975
HannaWH <hannawh@stud.ntnu.no>
2019-03-28 16:43:19 +0100

    Masse drit

---
+++ source/fsm.c
@@ -0,0 +1,18 @@
+#include "elev.h"
+#include "fsm.h"
+
+
+
+void fsm_react_to_button(){
+
+
+    switch(floor) {
+        case(-1) : {
+            order_place_order(i, j, orders);
+        }
+        default : {
+
+        }
+    }
+
+}
--- source/main.c
+++ source/main.c
@@ -1,8 +1,23 @@
 #include "elev.h"
 #include <stdio.h>
 
+int orders[12];
+int button_pressed() {
+    int count = 0; 
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 4; j++){
+            if(elev_get_button_signal(i, j)) {
+                order_place_order(i, j, orders);
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
 
 int main() {
+    
     // Initialize hardware
     if (!elev_init()) {
         printf("Unable to initialize elevator hardware!\n");
@@ -13,19 +28,33 @@ int main() {
 
     elev_set_motor_direction(DIRN_UP);
 
+
     while (1) {
+        
+        int floor = elev_get_floor_sensor_signal();
+
+        if(timer_timeout()) {
+            fsm_timeout();
+        }
+        if(button_pressed()){
+            fsm_register_button();
+        }
+        if(elev_get_floor_sensor_signal()){
+            fsm_in_floor();
+        }
+        if(elev_get_stop_signal()){
+            fsm_emergency();
+        }
+    
+
+
         // Change direction when we reach top/bottom floor
         if (elev_get_floor_sensor_signal() == N_FLOORS - 1) {
             elev_set_motor_direction(DIRN_DOWN);
         } else if (elev_get_floor_sensor_signal() == 0) {
             elev_set_motor_direction(DIRN_UP);
-        }
+        }    
 
-        // Stop elevator and exit program if the stop button is pressed
-        if (elev_get_stop_signal()) {
-            elev_set_motor_direction(DIRN_STOP);
-            break;
-        }
     }
 
     return 0;
